4d8dfe16a88959eacdc23d427fe59c7ef8852797
Ami Fischman <ami@fischman.org>
2025-02-24 15:58:25 -0800

    Avoid deepcopy'ing Shape.topo_parent.
    Speeds up benchy example from 27s to 5.5s.

--- src/build123d/topology/shape_core.py
+++ src/build123d/topology/shape_core.py
@@ -866,7 +866,10 @@ class Shape(NodeMixin, Generic[TOPODS]):
         if self.wrapped is not None:
             memo[id(self.wrapped)] = downcast(BRepBuilderAPI_Copy(self.wrapped).Shape())
         for key, value in self.__dict__.items():
-            setattr(result, key, copy.deepcopy(value, memo))
+            if key == 'topo_parent':
+                result.topo_parent = value
+            else:
+                setattr(result, key, copy.deepcopy(value, memo))
             if key == "joints":
                 for joint in result.joints.values():
                     joint.parent = result
