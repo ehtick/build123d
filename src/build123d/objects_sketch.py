1e1c81a09364d00259a20f4962835dd535da06f2
Jonathan Wagenet <jonathanwagenet@gmail.com>
2025-04-06 23:44:45 -0400

    SlotOverall: remove width != height else branch to make circle. width <= height ValueError makes this branch inaccessible.

--- src/build123d/objects_sketch.py
+++ src/build123d/objects_sketch.py
@@ -521,17 +521,14 @@ class SlotOverall(BaseSketchObject):
         self.width = width
         self.slot_height = height
 
-        if width != height:
-            face = Face(
-                Wire(
-                    [
-                        Edge.make_line(Vector(-width / 2 + height / 2, 0, 0), Vector()),
-                        Edge.make_line(Vector(), Vector(+width / 2 - height / 2, 0, 0)),
-                    ]
-                ).offset_2d(height / 2)
-            )
-        else:
-            face = cast(Face, Circle(width / 2, mode=mode).face())
+        face = Face(
+            Wire(
+                [
+                    Edge.make_line(Vector(-width / 2 + height / 2, 0, 0), Vector()),
+                    Edge.make_line(Vector(), Vector(+width / 2 - height / 2, 0, 0)),
+                ]
+            ).offset_2d(height / 2)
+        )
         super().__init__(face, rotation, align, mode)
 
 
